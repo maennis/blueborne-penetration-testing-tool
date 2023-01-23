2a1c9fde0bc1e88fd4eebf876087b240fea5ba06
maennis <mark.andrew.ennis@gmail.com>
2023-01-23 09:18:35 -0800

    Added main loop

--- src/main.c
+++ src/main.c
@@ -1,13 +1,46 @@
+#include <signal.h>
+#include <stdlib.h>
 #include <syslog.h>
 
 // Custom Imports
 #include "logger.h"
 
+#define NULL            0
 #define PROGRAM_NAME    "bluebornepentesttool"
+#define TRUE            1
+
+_Noreturn void cleanup(void);
+void set_sigaction(void);
+void setup(void);
 
 int main(int argc, char**argv) {
+    setup();
+    while(TRUE)
+    {
+
+    }
+    return 0;
+}
+
+_Noreturn void cleanup(void)
+{
+    logger_close();
+    systemlog(LOG_AUTH | LOG_INFO, "Shutting down %s", PROGRAM_NAME);
+    exit(EXIT_SUCCESS);
+}
+
+void set_sigaction(void)
+{
+    struct sigaction sigact;
+    sigact.sa_handler = cleanup;
+    sigact.sa_flags = 0;
+    if ((sigemptyset (&sigact.sa_mask) == -1 || sigaction (SIGINT, &sigact, NULL) == -1))
+        systemlog(LOG_AUTH | LOG_ERR, "Error setting signact");
+}
+
+void setup(void)
+{
+    set_sigaction();
     logger_init(PROGRAM_NAME);
     systemlog(LOG_AUTH | LOG_INFO, "%s started", PROGRAM_NAME);
-    logger_close();
-    return 0;
 }
