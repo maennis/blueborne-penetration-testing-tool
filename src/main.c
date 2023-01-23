1d8929d6c48b97ce5fab357c25e92c7c975005c3
maennis <mark.andrew.ennis@gmail.com>
2023-01-23 12:20:05 -0800

    Processed devices are tracked in memory

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <signal.h>
 #include <stdlib.h>
+#include <string.h>
 #include <syslog.h>
 #include <unistd.h>
 
@@ -12,14 +13,15 @@
 #define TRUE            1
 
 _Noreturn void cleanup(int signal);
+void process_device(bdaddr_t *address, int *processed_bt_addresses, char processed_addresses[MAXNUMBTRESP][BLUETOOTHADDRESSLEN]);
 void set_sigaction(void);
 void setup(void);
 
 int main(int argc, char**argv) {
     struct bluetooth_connection_info bt_info;
-    bdaddr_t *bt_address_list;
-    char btaddr_s[BLUETOOTHADDRESSLEN] = { 0 };
-    int i, responses = 0;
+    bdaddr_t *bt_address_list, btaddr;
+    char processed_bt_addresses[MAXNUMBTRESP][BLUETOOTHADDRESSLEN], btaddr_s[BLUETOOTHADDRESSLEN] = { 0 };
+    int i, responses, num_processed_bt_address = 0;
     setup();
 
     if ((bt_info.device_id = get_bluetooth_device_id()) < 0)
@@ -35,7 +37,7 @@ int main(int argc, char**argv) {
     }
 
     bt_address_list = (bdaddr_t *) malloc(sizeof(bdaddr_t) * MAXNUMBTRESP);
-    
+    memset(processed_bt_addresses, 0, MAXNUMBTRESP * BLUETOOTHADDRESSLEN);
     while(TRUE)
     {
         systemlog(LOG_AUTH | LOG_INFO, "Begining HCI inquiry");
@@ -43,10 +45,10 @@ int main(int argc, char**argv) {
         systemlog(LOG_AUTH | LOG_INFO, "Concluded inquiry.  Found %d devices", responses);
         for (i = 0; i < responses; i++)
         {
-            ba2str((bt_address_list + i), btaddr_s);
-            systemlog(LOG_AUTH | LOG_INFO, "Device %d address: %s", i, btaddr_s);
+            memcpy(&btaddr, bt_address_list + i, sizeof(bdaddr_t));
+            process_device(&btaddr, &num_processed_bt_address, processed_bt_addresses);
         }
-
+        systemlog(LOG_AUTH | LOG_INFO, "Finished processing devices", responses);
         sleep(POLL_INTERVAL);
     }
     return 0;
@@ -57,6 +59,19 @@ _Noreturn void cleanup(int signal)
     logger_close();
     systemlog(LOG_AUTH | LOG_INFO, "Shutting down %s", PROGRAM_NAME);
     exit(EXIT_SUCCESS);
+}
+
+void process_device(bdaddr_t *address, int *processed_bt_addresses, char processed_addresses[MAXNUMBTRESP][BLUETOOTHADDRESSLEN])
+{
+    int i;
+    char btaddr_s[BLUETOOTHADDRESSLEN] = { 0 };
+    ba2str(address, btaddr_s);
+    for (i = 0; i < *processed_bt_addresses; i++)
+        if (strcmp(btaddr_s, processed_addresses[i]) == 0)
+            return;
+    systemlog(LOG_AUTH | LOG_INFO, "Processing device with address %s address", btaddr_s);
+    strcpy(processed_addresses[*processed_bt_addresses], btaddr_s);
+    (*processed_bt_addresses)++;
 }
 
 void set_sigaction(void)
@@ -73,5 +88,4 @@ void setup(void)
     set_sigaction();
     logger_init(PROGRAM_NAME);
     systemlog(LOG_AUTH | LOG_INFO, "%s started", PROGRAM_NAME);
-
 }
