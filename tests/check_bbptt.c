23df3a2d08ee0de16e80004f4a9029c7087d3fbe
maennis <mark.andrew.ennis@gmail.com>
2023-01-23 10:15:35 -0800

    Added an test case for valid hci socket creation

--- tests/check_bbptt.c
+++ tests/check_bbptt.c
@@ -23,21 +23,35 @@ START_TEST(test_open_bluetooth_device_invalid_id)
 }
 END_TEST
 
+START_TEST(test_open_bluetooth_device_valid_id)
+{
+    int hci_socket, device_id;
+    device_id = get_bluetooth_device_id();
+    ck_assert_msg(device_id >= 0, "Invalid bluetooth device ID. A bluetooth adapter may not be available.");
+    hci_socket = open_bluetooth_device(device_id);
+    ck_assert_int_ge(hci_socket, 0);
+}
+END_TEST
+
 Suite * bbptt_suite(void)
 {
     Suite *s;
-    TCase *tc_setup, *tc_bluetooth_open_device_invalid_id;
+    TCase *tc_setup, *tc_bluetooth_open_device_invalid_id, *tc_bluetooth_open_device_valid_id;
     s = suite_create("BBPTT");
 
     // Set up
     tc_setup = tcase_create("Setup");
     // Bluetooth
     tc_bluetooth_open_device_invalid_id = tcase_create("open_bluetooth_device invalid id");
+    tc_bluetooth_open_device_valid_id = tcase_create("open_bluetooth_device invalid id");
 
     tcase_add_test(tc_setup, test_logger_run);
     suite_add_tcase(s, tc_setup);
     tcase_add_test(tc_bluetooth_open_device_invalid_id, test_open_bluetooth_device_invalid_id);
     suite_add_tcase(s, tc_bluetooth_open_device_invalid_id);
+    tcase_add_test(tc_bluetooth_open_device_valid_id, test_open_bluetooth_device_valid_id);
+    suite_add_tcase(s, tc_bluetooth_open_device_valid_id);
+    
     return s;
 }
 
